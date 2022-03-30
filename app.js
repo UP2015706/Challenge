cdad9803fab66ad3d92ca0c2f8ad6af50d715e59
UP2015706 <92105890+UP2015706@users.noreply.github.com>
2022-03-30 15:24:54 +0100

    Challenge Progress

--- app.js
+++ app.js
@@ -11,9 +11,21 @@ app.get('/stocks', async (req, res) => {
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
+  // const { params: { symbol } } = req
+  // const data = await stocks.getStockPoints(symbol, new Date())
+  // res.send(data)
+
+try {
   const { params: { symbol } } = req
   const data = await stocks.getStockPoints(symbol, new Date())
   res.send(data)
+  
+} catch (error) {
+  console.log(error)
+  res.sendStatus(404)
+}
 })
 
 app.listen(3000, () => console.log('Server is running!'))
+
+
--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('chart')
 const ctx = canvas.getContext('2d')
 
+
+
 function drawLine (start, end, style) {
   ctx.beginPath()
   ctx.strokeStyle = style || 'black'
@@ -22,3 +24,58 @@ drawTriangle([35, 50], [65, 50], [50, 35])
 
 drawLine([50, 550], [950, 550])
 drawTriangle([950, 535], [950, 565], [965, 550])
+
+
+
+//Getting hold of DOM elements.
+let spinnerObject = document.querySelector('.spinner')
+let doButton = document.getElementById('getStock');
+let input = document.getElementById('selectStock');
+
+//Function that fetches the initial list which stores the stock "FB" "AAPL" etc.
+async function fetchList (){
+  const listData = await fetch('/stocks') 
+  const response = await listData.json()
+  console.log(response)
+}
+
+// Event listener for the button.
+doButton.addEventListener('click', async () =>{
+  await fetchData() 
+})
+
+//Fetching the data within the object adding input in order to access the specific type we want.
+
+async function fetchData (){
+let logInput = input.value.toUpperCase();
+const listData = await fetch(`/stocks/${logInput}`) //Template Literals
+
+
+  if (!listData.ok) {
+    // handle error here
+    console.log('No Data Available')
+  }
+  // handle the response
+  const data = await listData.json();
+  //Transforming the timestamps and value into actual readable data.
+    data.forEach(element => {
+      let stockPoint = (Number.parseFloat(element.value).toFixed(2))
+      let timestamp = element.timestamp
+      let date = new Date(timestamp);
+  
+      console.log(`
+          ${logInput}: '£'${stockPoint});
+          Date: ${date} 
+          `)
+
+    
+    });
+  spinnerObject.style.visibility = "hidden";
+}
+
+
+//Waiting for the window to load in order to fetch.
+async function innit(){
+await fetchList();
+}
+ window.addEventListener('load',innit);
